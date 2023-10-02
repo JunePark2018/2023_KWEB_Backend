cd32eb6052fdc3a1fc801f83ede51ded236a52f3
June Park / 박준 <35623589+JunePark2018@users.noreply.github.com>
2023-10-02 15:00:10 +0900

    2.1 과제

---
+++ 과제/Chapter 2.4/Exercise 1/cases.js
@@ -0,0 +1,23 @@
+const permutation = (n, r) => {
+  let result = 1;
+  for (let i = n; i > n - r; i--) result *= i;
+  return result;
+};
+
+const combination = (n, r) => {
+  let result = permutation(n, r);
+  for (let i = 2; i <= r; i++) result /= i;
+  return result;
+};
+
+const multiPermutation = (n, r) => n ** r;
+
+const multiCombination = (n, r) => combination(n + r - 1, r);
+
+
+module.exports = {
+  permutaion,
+  combination,
+  multiPermutation,
+  multiCombination
+};
